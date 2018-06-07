a2e379375089e3bbf59adb7544b05d57dc65cceb
Jules <julianrosser91@gmail.com>
2018-06-07 17:21:52 +0100

    make fields observable

--- app/src/main/java/app/julianrosser/androidmvvm/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/app/julianrosser/androidmvvm/viewmodel/CalculatorViewModel.kt
@@ -1,12 +1,13 @@
 package app.julianrosser.androidmvvm.viewmodel
 
+import android.databinding.BaseObservable
+import android.util.Log
 import app.julianrosser.androidmvvm.model.Calculator
 import app.julianrosser.androidmvvm.model.WageChange
 
-class CalculatorViewModel(val calculator: Calculator = Calculator()) {
+class CalculatorViewModel(val calculator: Calculator = Calculator()): BaseObservable() {
 
     var inputCurrentWage = ""
-
     var inputNewWage = ""
 
     var wageCalculation = WageChange()
@@ -15,6 +16,8 @@ class CalculatorViewModel(val calculator: Calculator = Calculator()) {
 
         inputCurrentWage.toIntOrNull()?.let { currentWageInt ->
             inputNewWage.toIntOrNull()?.let { newWageInt ->
+
+                Log.i("ttt", "values: $inputCurrentWage  -> $inputNewWage ")
 
                 wageCalculation = calculator.calculateWageChange(currentWageInt, newWageInt)
             }
