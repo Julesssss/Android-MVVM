bfef9087325cd80fbb3b36c07c5fea722b9cb5f0
Jules <julianrosser91@gmail.com>
2018-06-07 17:15:11 +0100

    use data binding to connect button to viewmodel

--- app/src/main/java/app/julianrosser/androidmvvm/view/MainActivity.kt
+++ app/src/main/java/app/julianrosser/androidmvvm/view/MainActivity.kt
@@ -1,13 +1,21 @@
 package app.julianrosser.androidmvvm.view
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import app.julianrosser.androidmvvm.R
+import app.julianrosser.androidmvvm.databinding.ActivityMainBinding
+import app.julianrosser.androidmvvm.viewmodel.CalculatorViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.vm = CalculatorViewModel()
+
     }
 }
