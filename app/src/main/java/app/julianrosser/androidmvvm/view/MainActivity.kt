89682eb594871ac67a388a0290aa8e99a12f6b16
Jules <julianrosser91@gmail.com>
2018-06-28 15:00:58 +0100

    document MainActivity with comments

--- app/src/main/java/app/julianrosser/androidmvvm/view/MainActivity.kt
+++ app/src/main/java/app/julianrosser/androidmvvm/view/MainActivity.kt
@@ -1,22 +1,28 @@
 package app.julianrosser.androidmvvm.view
 
-import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import app.julianrosser.androidmvvm.R
 import app.julianrosser.androidmvvm.databinding.ActivityMainBinding
 import app.julianrosser.androidmvvm.viewmodel.CalculatorViewModel
 
+/**
+ * The main application activity. Allows users to calculate wage changes with the use of CalculatorViewModel
+ */
 class MainActivity : AppCompatActivity() {
 
+    // Activity binding used to link views to persistent ViewModel and allows 2 way binding link
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // sets layout and returns binding associated with it
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // link CalculatorViewModel to activity binding
         binding.vm = ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
     }
 }
