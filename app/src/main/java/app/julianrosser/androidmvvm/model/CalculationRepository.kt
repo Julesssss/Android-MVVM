753171de75412d0b6c3e098d5c8ba99ba7b46447
Jules <julianrosser91@gmail.com>
2018-06-28 17:34:02 +0100

    add LiveData accessor function to CalculatorRepository and document class

--- app/src/main/java/app/julianrosser/androidmvvm/model/CalculationRepository.kt
+++ app/src/main/java/app/julianrosser/androidmvvm/model/CalculationRepository.kt
@@ -1,13 +1,37 @@
 package app.julianrosser.androidmvvm.model
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+
+/**
+ * Basic repository for temporarily persisting WageChanges. Keeps wage changes in memory and
+ * provides a variety of saving, access methods.
+ */
 class CalculationRepository {
 
+    // Temporary data storage
     private val memoryStorage = mutableMapOf<String, WageChange>()
 
+    /**
+     * Add a WageChange calculation to the storage map
+     */
     fun saveWageChange(wageChange: WageChange) {
         memoryStorage[wageChange.title] = wageChange
     }
 
-    fun loadWageChange(wageChangeTitle: String) = memoryStorage[wageChangeTitle]
+    /**
+     * Load a single WageChange calculation by name
+     */
+    fun loadWageChangeByName(wageChangeTitle: String) = memoryStorage[wageChangeTitle]
+
+    /**
+     * Retrieve a LiveData observable containing all WageChange calculations. Is aware of
+     * subscriber's lifecycle.
+     */
+    fun loadObservableWageChanges(): LiveData<List<WageChange>> {
+        val liveData = MutableLiveData<List<WageChange>>()
+        liveData.value = memoryStorage.values.toList()
+        return liveData
+    }
 
 }
--- app/src/test/java/app/julianrosser/androidmvvm/model/CalculationRepositoryTest.kt
+++ app/src/test/java/app/julianrosser/androidmvvm/model/CalculationRepositoryTest.kt
@@ -1,12 +1,19 @@
 package app.julianrosser.androidmvvm.model
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Before
 import org.junit.Test
 import junit.framework.Assert.assertEquals
+import org.junit.Rule
+import org.junit.rules.TestRule
 
 class CalculationRepositoryTest {
 
-     lateinit var repository: CalculationRepository
+    // Necessary in order to test live data architecture component
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    lateinit var repository: CalculationRepository
 
     @Before
     fun setup() {
@@ -19,7 +26,20 @@ class CalculationRepositoryTest {
 
         repository.saveWageChange(wageChange)
 
-        assertEquals(wageChange, repository.loadWageChange(wageChange.title))
+        assertEquals(wageChange, repository.loadWageChangeByName(wageChange.title))
+    }
+
+    @Test
+    fun `load saved wage change`() {
+        val wageChange = WageChange("test wage", 10000, 15000, 5000, 100)
+
+        repository.saveWageChange(wageChange)
+        repository.saveWageChange(wageChange.copy(title = "test wage 2"))
+
+
+        repository.loadObservableWageChanges().observeForever { tipCalculations ->
+            assertEquals(2, tipCalculations?.size)
+        }
     }
 
 }
